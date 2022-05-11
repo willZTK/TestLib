5524c62561e0bfa457e6e1fee2ef107b126b0ae8
will <1753552195@qq.com>
2022-05-11 17:10:56 +0800

    first commit

---
+++ Example/TestLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TestLib
+//
+//  Created by will on 05/11/2022.
+//  Copyright (c) 2022 will. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ TestLib/Classes/TestCode.swift
@@ -0,0 +1,14 @@
+//
+//  TestCode.swift
+//  Tips-Swift
+//
+//  Created by kl on 2022/5/11.
+//
+
+import UIKit
+
+public class TestCode: NSObject {
+    public func printHelloWorld() {
+        print("hello world!")
+    }
+}
